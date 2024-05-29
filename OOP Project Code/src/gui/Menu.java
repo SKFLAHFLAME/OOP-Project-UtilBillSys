ec9f37289c57031c09cf4660b4ec3806c9b98c57
SKFLAHFLAME <samuelkoh3@gmail.com>
2024-05-29 22:38:24 +0800

    new2

--- OOP Project Code/src/gui/Menu.java
+++ OOP Project Code/src/gui/Menu.java
@@ -2,6 +2,13 @@ package gui;
 
 import javax.swing.JPanel;
 
+import controller.MainFrame;
+
 public class Menu extends JPanel{
+    MainFrame main;
+
+    public Menu(MainFrame m){
+        this.main = m;
+    }
 
 }
